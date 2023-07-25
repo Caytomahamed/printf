6bc8c1fa0a11d47a02a21852d0be3b885e2fad12
Aidarous <aidarous1143@gmail.com>
2023-07-25 18:02:17 +0300

    🚗 betty

--- _printf.c
+++ _printf.c
@@ -1,74 +1,54 @@
 #include "main.h"
-int convert_matchs(va_list *args, char c);
 /**
- * _printf - prints formatted output to stdout
- * @format: format string containing zero or more directives
+ * _printf - prints output according to a format
+ * @format: the format string
  *
- * Return: the number of characters printed (excluding the null byte)
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings)
  */
-int _printf(const char *const format, ...)
+int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, len = 0;
-
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-	while (format[i] != '\0')
+	int count = 0;
+
+	while (*format != '\0')
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-			len += convert_matchs(&args, format[i]);
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			len++;
+			putchar(*format);
+			count++;
 		}
-		i++;
+		format++;
 	}
+
 	va_end(args);
 
-	return (len);
-}
-
-/**
- * convert_matchs - selects the correct function to perform the conversion
- * @args: va_list containing the arguments to convert
- * @c: conversion specifier to look for
- *
- * Return: the number of characters printed by the conversion function
- */
-int convert_matchs(va_list *args, char c)
-{
-	convert_match funcs[] = {
-		{'c', printf_char},
-		{'s', printf_string},
-		{'%', printf_37},
-		{'i', printf_int},
-		{'d', printf_dec},
-		{'r', printf_srev},
-		{'R', printf_rot13},
-		{'b', printf_bin},
-		{'u', printf_unsigned},
-		{'o', printf_oct},
-		{'x', printf_hex},
-		{'X', printf_HEX},
-		{'S', printf_exclusive_string},
-		{'p', printf_pointer},
-		{0, NULL}};
-	int i = 0;
-
-	while (funcs[i].f != NULL)
-	{
-		if (funcs[i].id == c)
-			return (funcs[i].f(args));
-		i++;
-	}
-
-	return (-1);
+	return (count);
 }
