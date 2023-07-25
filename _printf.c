45e8ac5e27c88b75b46056e5af613879fb3ce4a5
Aidarous <aidarous1143@gmail.com>
2023-07-25 16:01:21 +0300

    🚨 betty

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - prints formatted output to stdout
  * @format: format string containing zero or more directives
@@ -10,25 +9,18 @@ int _printf(const char *format, ...)
 {
 	int count;
 	char *str;
-
 	va_list arg;
 
 	va_start(arg, format);
-
 	count = 0;
-
 	while (*format != '\0')
 	{
-
 		switch (*format)
 		{
 		case 'c':
-		{
 			_putchar(va_arg(arg, int));
 			break;
-		}
 		case 's':
-		{
 			str = va_arg(arg, char *);
 			while (*str != '\0')
 			{
@@ -36,17 +28,13 @@ int _printf(const char *format, ...)
 				str++;
 			}
 			break;
-		}
 		case '%':
-		{
 			_putchar('%');
 			break;
-		}
 		}
 		format++;
 		count++;
 	}
-
 	va_end(arg);
 	return (count);
 }
